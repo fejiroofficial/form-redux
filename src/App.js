82769fdec4552a06adcaaa6ce69632e1e00e7e2e
Fejiro Gospel <fejiro.gospel@gmail.com>
2021-09-08 14:09:37 +0100

    Skeletal set up for react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <input type="text" placeholder="Enter your name" />
+          <button>Submit</button>
+        </form>
+        <div className="App-display">{name}</div>
+      </div>
     </div>
   );
 }
